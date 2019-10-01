c64e37741b03d0c6e99c67b20e186026f7cd5eee
Igor Trindade <igorhideki@gmail.com>
2019-10-01 05:33:50 -0400

    add styles global

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
       <router-link to="/">
         Home
       </router-link> |
+      <router-link to="/login">
+        Login
+      </router-link> |
+      <router-link to="/register">
+        Register
+      </router-link> |
       <router-link to="/about">
         About
       </router-link>
@@ -13,4 +19,40 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+*:focus {
+  outline: 0
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  -weblit-font-smoothing: antialiased;
+}
+
+body, input, button {
+  font: 14px 'Roboto', sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+button {
+  cursor: pointer;
+}
 </style>
