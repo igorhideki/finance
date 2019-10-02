25c15fed94c4ab63d624d5faa412297a7d20dabd
Igor Trindade <igorhideki@gmail.com>
2019-10-02 06:24:57 -0400

    update style de LayoutAuth

--- src/layouts/LayoutAuth.vue
+++ src/layouts/LayoutAuth.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     functional
-    class="wrapper"
+    class="layout-auth"
   >
     <div class="content">
       <slot />
@@ -16,77 +16,77 @@ export default {
 </script>
 
 <style lang="scss">
-.wrapper {
+.layout-auth {
   height: 100%;
   background: $gradient-linear;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  .content {
+    width: 100%;
+    max-width: 316px;
+    text-align: center;
+
+    h1 {
+      color: $primary;
+      text-transform: uppercase;
+    }
+
+    form {
+      margin-top: 30px;
+      display: flex;
+      flex-direction: column;
+
+      input {
+        background: rgba(0, 0, 0, 0.15);
+        border: 0;
+        border-radius: 4px;
+        height: 44px;
+        padding: 0 15px;
+        color: #fff;
+        margin: 0 0 10px;
+
+        &::placeholder {
+          color: rgba(255, 255, 255, 0.7);
+        }
+      }
+
+      > span {
+        margin-bottom: 10px;
+        align-self: flex-start;
+        color: $danger;
+      }
+
+      button {
+        margin: 5px 0 0;
+        height: 44px;
+        background: $primary;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        transition: background 0.2s;
+
+        &:hover {
+          background: $primary-dark;
+        }
+      }
+
+      a {
+        color: #fff;
+        margin-top: 15px;
+        font-size: 16px;
+        opacity: 0.8;
+        transition: opacity 0.2s;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
 }
 
-.content {
-  width: 100%;
-  max-width: 316px;
-  text-align: center;
-
-  h1 {
-    color: $primary;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-  }
-
-  form {
-    margin-top: 30px;
-    display: flex;
-    flex-direction: column;
-
-    input {
-      background: rgba(0, 0, 0, 0.15);
-      border: 0;
-      border-radius: 4px;
-      height: 44px;
-      padding: 0 15px;
-      color: #fff;
-      margin: 0 0 10px;
-
-      &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
-      }
-    }
-
-    > span {
-      margin-bottom: 10px;
-      align-self: flex-start;
-      color: $danger;
-    }
-
-    button {
-      margin: 5px 0 0;
-      height: 44px;
-      background: $primary;
-      font-weight: bold;
-      color: #fff;
-      border: 0;
-      border-radius: 4px;
-      font-size: 16px;
-      transition: background 0.2s;
-
-      &:hover {
-        background: $primary-dark;
-      }
-    }
-
-    a {
-      color: #fff;
-      margin-top: 15px;
-      font-size: 16px;
-      opacity: 0.8;
-      transition: opacity 0.2s;
-
-      &:hover {
-        opacity: 1;
-      }
-    }
-  }
-}
 </style>
