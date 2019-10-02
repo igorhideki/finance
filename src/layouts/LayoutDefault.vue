5a51c137f5596cc18754384761676f230bca94f6
Igor Trindade <igorhideki@gmail.com>
2019-10-02 06:26:27 -0400

    add LayoutDefault

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    functional
+    class="layout-default"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.layout-default {
+  height: 100%;
+  background: $gradient-linear;
+}
+</style>
