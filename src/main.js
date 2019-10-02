37408c32dffc85ab33f0dee43cf31ee622c74699
Igor Trindade <igorhideki@gmail.com>
2019-10-01 22:19:55 -0400

    add font awesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+library.add(faSpinner)
+
+Vue.component('fa-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
