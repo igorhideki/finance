6de2a0cafb5fd2429e99049825b4a6fd759b2a4d
Igor Trindade <igorhideki@gmail.com>
2019-10-03 06:23:11 -0400

    add componente badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,43 @@
+<template>
+  <span
+    class="badge"
+    :class="state"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+  props: {
+    state: {
+      type: String,
+      required: true,
+      validator (state) {
+        const states = ['success', 'danger', 'info']
+
+        return states.includes(state)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scpoed>
+.badge {
+  display: inline-block;
+  padding: 2px 6px;
+  border-radius: 2px;
+  color: #fff;
+  font-size: 12px;
+
+  &.danger {
+    background: $danger;
+  }
+
+  &.success {
+    background: $success;
+  }
+}
+</style>
