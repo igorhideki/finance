69cb1f17a2ffb480ea063bd531797a6da296b422
Igor Trindade <igorhideki@gmail.com>
2019-10-03 06:23:18 -0400

    add componente card

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background: #fff;
+  padding: 16px;
+  color: $gray;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+</style>
