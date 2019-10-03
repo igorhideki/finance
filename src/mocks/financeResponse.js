25d7095ab51d1fce9a4820722283c0c4d08c695d
Igor Trindade <igorhideki@gmail.com>
2019-10-03 06:24:37 -0400

    add mock resposta api

---
+++ src/mocks/financeResponse.js
@@ -0,0 +1,149 @@
+export default {
+  'by': 'default',
+  'valid_key': true,
+  'results': {
+    'currencies': {
+      'source': 'BRL',
+      'USD': {
+        'name': 'Dollar',
+        'buy': 4.1344,
+        'sell': 4.1302,
+        'variation': -0.673
+      },
+      'EUR': {
+        'name': 'Euro',
+        'buy': 4.5306,
+        'sell': 4.5262,
+        'variation': -0.437
+      },
+      'GBP': {
+        'name': 'Pound Sterling',
+        'buy': 5.0873,
+        'sell': null,
+        'variation': -0.543
+      },
+      'ARS': {
+        'name': 'Argentine Peso',
+        'buy': 0.0714,
+        'sell': null,
+        'variation': -1.108
+      },
+      'BTC': {
+        'name': 'Bitcoin',
+        'buy': 36382.077,
+        'sell': 36382.077,
+        'variation': 0.568
+      }
+    },
+    'stocks': {
+      'IBOVESPA': {
+        'name': 'BM&F BOVESPA',
+        'location': 'Sao Paulo, Brazil',
+        'points': 101031.438,
+        'variation': -2.9
+      },
+      'NASDAQ': {
+        'name': 'NASDAQ Stock Market',
+        'location': 'New York City, United States',
+        'points': 7785.25,
+        'variation': -1.56
+      },
+      'CAC': {
+        'name': 'CAC 40',
+        'location': 'Paris, French',
+        'variation': 0.65
+      },
+      'NIKKEI': {
+        'name': 'Nikkei 225',
+        'location': 'Tokyo, Japan',
+        'variation': -2.01
+      }
+    },
+    'available_sources': [
+      'BRL'
+    ],
+    'bitcoin': {
+      'blockchain_info': {
+        'name': 'Blockchain.info',
+        'format': [
+          'USD',
+          'en_US'
+        ],
+        'last': 8301.74,
+        'buy': 8301.74,
+        'sell': 8301.74,
+        'variation': 0.568
+      },
+      'coinbase': {
+        'name': 'Coinbase',
+        'format': [
+          'USD',
+          'en_US'
+        ],
+        'last': 8290.885,
+        'variation': 0.409
+      },
+      'bitstamp': {
+        'name': 'BitStamp',
+        'format': [
+          'USD',
+          'en_US'
+        ],
+        'last': 8304.47,
+        'buy': 8302.23,
+        'sell': 8301.0,
+        'variation': 0.5
+      },
+      'foxbit': {
+        'name': 'FoxBit',
+        'format': [
+          'BRL',
+          'pt_BR'
+        ],
+        'last': 35069.57,
+        'variation': 1.24
+      },
+      'mercadobitcoin': {
+        'name': 'Mercado Bitcoin',
+        'format': [
+          'BRL',
+          'pt_BR'
+        ],
+        'last': 34846.00001,
+        'buy': 34846.00001,
+        'sell': 35096.46926,
+        'variation': 0.421
+      },
+      'omnitrade': {
+        'name': 'OmniTrade',
+        'format': [
+          'BRL',
+          'pt_BR'
+        ],
+        'last': 35280.0,
+        'buy': 35170.0,
+        'sell': 35700.0,
+        'variation': 0.199
+      },
+      'xdex': {
+        'name': 'XDEX',
+        'format': [
+          'BRL',
+          'pt_BR'
+        ],
+        'last': 35180.0,
+        'variation': 0.914
+      }
+    },
+    'taxes': [
+      {
+        'date': '2019-09-25',
+        'cdi': 5.4,
+        'selic': 5.4,
+        'daily_factor': 1.00020872
+      }
+    ]
+  },
+  'execution_time': 0.0,
+  'from_cache': true
+}
